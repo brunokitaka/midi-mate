2d0a8e53a34ac3078c3145481e8f369cf3b23efa
brunokitaka <brunokitaka@gmail.com>
2020-05-28 12:28:37 -0300

    routes

---
+++ Backend/app/routes/web/ideas.js
@@ -0,0 +1,44 @@
+/**
+ * MODULES
+ */
+const { check, validationResult } = require('express-validator'); /* Request data validator. */
+
+module.exports = function (app) {
+    /**
+     * LIST IDEAS:
+     * Returns list ideas page.
+     */
+    app.get('/listIdeas', function (req, res) {
+        /* Gets isValid() instance. */
+        const isValid = app.app.controllers.web.login.isValid;
+
+        /* Checks route permission. */
+        if (req.session.token != undefined && isValid(req.session.email + req.session.idUser.toString(), req.session.token)) {
+            res.render("./ideas/listIdeas");
+        }
+        /* If session is not valid, retuns login page. */
+        else {
+            res.render("./user/login")
+            return;
+        }
+    });
+
+    /**
+     * SELECT USER IDEAS:
+     * Selects all user's ideas.
+     */
+    app.post('/selectUserIdeas', function (req, res) {
+        /* Gets isValid() instance. */
+        const isValid = app.app.controllers.web.login.isValid;
+
+        /* Checks route permission. */
+        if (req.session.token != undefined && isValid(req.session.email + req.session.idUser.toString(), req.session.token)) {
+            app.app.controllers.web.ideas.selectUserIdeas(app, req, res);
+        }
+        /* If session is not valid. */
+        else {
+            res.send({status: "error", msg: "Access denied!"});
+            return;
+        }
+    });
+}
